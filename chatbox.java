61aabfe3c790c94879adb857f3a2a1f87363fc4a
GumpiliRanjith <ranjithgumpilli@gmail.com>
2025-10-18 23:01:13 +0530

    add new file

--- chatbox.java
+++ chatbox.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 import java.util.Scanner;
-public class chatbox.java{
+public class chatbox{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         HashMap<String, String> knowledgeBase = new HashMap<>();
